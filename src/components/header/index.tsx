cffb4b31914fbd829da924b515e0b52bfafe66c3
Adrian-kx <adriankxavier@gmail.com>
2023-10-12 22:38:31 -0300

    up

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,11 @@
-import { Link } from "react-router-dom";
 import styles from "./header.module.css";
 import Logo from "./../../assets/cryptoview-low-resolution-logo-white-on-transparent-background.svg";
 export function Header() {
   return (
     <header className={styles.container}>
-      <Link className={styles.logo} to="/">
-        <img src={Logo} />
-      </Link>
+      <a className={styles.logo} href="/">
+        <img alt="logo" src={Logo} />
+      </a>
     </header>
   );
 }
